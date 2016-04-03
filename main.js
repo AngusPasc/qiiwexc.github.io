53eec42e55c744eb9d2770e79a92399e23ed568c
qiiwexc <qiiwexc@icloud.com>
2016-04-03 21:06:25 +0300

    Added files via upload

--- main.js
+++ main.js
@@ -176,7 +176,7 @@ $('form').submit(function (event) {
 
   // notify if search box is empty, otherwise - go to devid.info
   if (text.replace(new RegExp(' ', 'g'), '').length < 1) {
-    alert('The search field is empty!');
+    window.alert('The search field is empty!');
   } else {
     window.open('http://devid.info/search?text=' + text.replace(/&/g, '%26'));
   }
@@ -223,6 +223,39 @@ $('.content a').click(function (event) {
 /*
  * Run at startup
  */
+
+// Yandex Metric
+(function (d, w, c) {
+  (w[c] = w[c] || []).push(function () {
+    try {
+      w.yaCounter35198485 = new Ya.Metrika({
+        accurateTrackBounce: true,
+        clickmap: true,
+        ecommerce: 'dataLayer',
+        id: 35198485,
+        trackHash: true,
+        trackLinks: true,
+        webvisor: true
+      });
+    } catch (e) { }
+  });
+
+  var n = d.getElementsByTagName('script')[0];
+  var s = d.createElement('script');
+  var f = function () {
+    n.parentNode.insertBefore(s, n);
+  };
+
+  s.async = true;
+  s.src = 'http://mc.yandex.ru/metrika/watch.js';
+  s.type = 'text/javascript';
+
+  if (w.opera === '[object Opera]') {
+    d.addEventListener('DOMContentLoaded', f, false);
+  } else {
+    f();
+  }
+})(document, window, 'yandex_metrika_callbacks');
 
 // detect system architecture
 if (navigator.userAgent.indexOf('WOW64') !== -1 || navigator.userAgent.indexOf('Win64') !== -1) {
