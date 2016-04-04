5d14c364657ebb59478d088a09531769d720de63
qiiwexc <qiiwexc@icloud.com>
2016-04-04 22:17:59 +0300

    Finish design update

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var systemArchitecture;
+var systemLanguage;
+var systemVersion;
+
 // Yandex Metric
 (function (d, w, c) {
   (w[c] = w[c] || []).push(function () {
@@ -33,6 +37,47 @@
   }
 })(document, window, 'yandex_metrika_callbacks');
 
+// detect system architecture
+if (navigator.userAgent.indexOf('WOW64') !== -1 || navigator.userAgent.indexOf('Win64') !== -1) {
+  systemArchitecture = 64;
+} else {
+  systemArchitecture = 86;
+}
+
+// detect system language
+if (navigator.language.indexOf('lv') !== -1) {
+  systemLanguage = 'LV';
+} else if (navigator.language.indexOf('ru') !== -1) {
+  systemLanguage = 'RU';
+} else {
+  systemLanguage = 'EN';
+}
+
+// detect system version
+if (navigator.appVersion.indexOf('Windows NT 10.0') !== -1) {
+  systemVersion = 10;
+} else if (navigator.appVersion.indexOf('Windows NT 6.4') !== -1) {
+  systemVersion = 10;
+} else if (navigator.appVersion.indexOf('Windows NT 6.3') !== -1) {
+  systemVersion = 8.1;
+} else if (navigator.appVersion.indexOf('Windows NT 6.2') !== -1) {
+  systemVersion = 8;
+} else if (navigator.appVersion.indexOf('Windows NT 6.1') !== -1) {
+  systemVersion = 7;
+} else if (navigator.appVersion.indexOf('Windows NT 6.0') !== -1) {
+  systemVersion = 'Vista';
+} else if (navigator.appVersion.indexOf('Windows NT 5.2') !== -1) {
+  systemVersion = 'XP';
+} else if (navigator.appVersion.indexOf('Windows NT 5.1') !== -1) {
+  systemVersion = 'XP';
+} else if (navigator.appVersion.indexOf('Windows NT 5.0') !== -1) {
+  systemVersion = 2000;
+} else if (navigator.appVersion.indexOf('Windows NT 4.0') !== -1) {
+  systemVersion = 'NT';
+} else {
+  systemVersion = '?';
+}
+
 // search for drivers
 document.getElementsByTagName('form')[0].onsubmit = function (event) {
   var text = document.getElementsByTagName('input')[0].value;
@@ -45,3 +90,12 @@ document.getElementsByTagName('form')[0].onsubmit = function (event) {
     window.open('http://devid.info/search?text=' + text.replace(/&/g, '%26'));
   }
 };
+
+// display system information
+document.getElementsByClassName('info')[0].innerHTML = (systemVersion === '?' ? 'Unknown Operating System' : 'Windows ' + systemVersion) + ' x' + systemArchitecture + ' | ' + systemLanguage;
+
+// highlight suggested language
+var elements = document.getElementsByClassName(systemLanguage);
+for (var i = 0; i < elements.length; i++) {
+  elements[i].className = systemLanguage + ' suggest';
+}
