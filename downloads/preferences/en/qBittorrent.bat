fba4d5fe1c7565636678162853ce333a9aee6195
qiiwexc <qiiwexc@icloud.com>
2016-04-10 03:54:56 +0300

    Update qBitturrent preferences

--- downloads/preferences/en/qBittorrent.bat
+++ downloads/preferences/en/qBittorrent.bat
@@ -39,11 +39,18 @@ if not exist %target% mkdir %target%
 
 cd %target%
 
-echo [LegalNotice]> qBittorrent.ini
+echo [Application]> qBittorrent.ini
+(echo FileLogger\Age=1)>> qBittorrent.ini
+(echo FileLogger\AgeType=0)>> qBittorrent.ini
+echo FileLogger\Backup=true>> qBittorrent.ini
+echo FileLogger\DeleteOld=true>> qBittorrent.ini
+echo FileLogger\Enabled=false>> qBittorrent.ini
+(echo FileLogger\MaxSize=1)>> qBittorrent.ini
+echo [LegalNotice]>> qBittorrent.ini
 echo Accepted=true>> qBittorrent.ini
 echo [MainWindow]>> qBittorrent.ini
 echo geometry="@ByteArray(\x1\xd9\xd0\xcb\0\x2\0\0\xff\xff\xff\xf8\xff\xff\xff\xf8\0\0\x5]\0\0\x2\xdf\0\0\0\xe2\0\0\0^\0\0\x4s\0\0\x2\x90\0\0\0\0\x2\0\0\0\x5V)">> qBittorrent.ini
-echo qt5\vsplitterState=@ByteArray(\0\0\0\xff\0\0\0\x1\0\0\0\x2\0\0\0v\0\0\x4\xc9\x1\xff\xff\xff\xff\x1\0\0\0\x1\0)>> qBittorrent.ini
+echo qt5\vsplitterState="@ByteArray(\0\0\0\xff\0\0\0\x1\0\0\0\x2\0\0\0w\0\0\x4\xc8\x1\xff\xff\xff\xff\x1\0\0\0\x1\0)">> qBittorrent.ini
 echo [Preferences]>> qBittorrent.ini
 echo Advanced\AnnounceToAllTrackers=true>> qBittorrent.ini
 echo Advanced\confirmTorrentDeletion=true>> qBittorrent.ini
@@ -64,6 +71,7 @@ echo Bittorrent\uTP=true>> qBittorrent.ini
 echo Bittorrent\uTP_rate_limited=false>> qBittorrent.ini
 echo Connection\GlobalDLLimitAlt=-1>> qBittorrent.ini
 echo Connection\GlobalUPLimitAlt=-1>> qBittorrent.ini
+echo Connection\InterfaceListenIPv6=true>> qBittorrent.ini
 echo Connection\MaxHalfOpenConnec=-1>> qBittorrent.ini
 echo Connection\ResolvePeerCountries=true>> qBittorrent.ini
 echo Connection\ResolvePeerHostNames=true>> qBittorrent.ini
@@ -71,10 +79,11 @@ echo Connection\UPnP=true>> qBittorrent.ini
 echo Downloads\AppendLabel=true>> qBittorrent.ini
 (echo Downloads\DblClOnTorDl=0)>> qBittorrent.ini
 (echo Downloads\DblClOnTorFn=1)>> qBittorrent.ini
+echo Downloads\DiskWriteCacheSize=1536>> qBittorrent.ini
 echo Downloads\NewAdditionDialog=true>> qBittorrent.ini
 echo Downloads\NewAdditionDialogFront=true>> qBittorrent.ini
 echo Downloads\PreAllocation=true>> qBittorrent.ini
-(echo Downloads\SaveResumeDataInterval=3)>> qBittorrent.ini
+(echo Downloads\SaveResumeDataInterval=1)>> qBittorrent.ini
 echo Downloads\StartInPause=false>> qBittorrent.ini
 echo Downloads\UseIncompleteExtension=true>> qBittorrent.ini
 echo ExecutionLog\enabled=false>> qBittorrent.ini
@@ -83,10 +92,9 @@ echo General\CloseToTray=false>> qBittorrent.ini
 echo General\ExitConfirm=true>> qBittorrent.ini
 echo General\Locale=%lang%>> qBittorrent.ini
 echo General\MinimizeToTray=false>> qBittorrent.ini
-echo General\NoSplashScreen=true>> qBittorrent.ini
 echo General\ProgramNotification=true>> qBittorrent.ini
 echo General\PreventFromSuspend=true>> qBittorrent.ini
-echo General\RefreshInterval=1000>> qBittorrent.ini
+echo General\RefreshInterval=30>> qBittorrent.ini
 echo General\SpeedInTitleBar=true>> qBittorrent.ini
 echo General\StartMinimized=false>> qBittorrent.ini
 echo General\SystrayEnabled=false>> qBittorrent.ini
@@ -111,3 +119,6 @@ echo [TorrentProperties]>> qBittorrent.ini
 (echo CurrentTab=5)>> qBittorrent.ini
 echo SplitterSizes="71,542">> qBittorrent.ini
 echo Visible=true>> qBittorrent.ini
+echo Peers\qt5\PeerListState="@ByteArray(\0\0\0\xff\0\0\0\0\0\0\0\x1\0\0\0\x1\0\0\0\a\x1\0\0\0\0\0\0\0\0\0\0\0\xe\0\x30\0\0\0\x2\0\0\0\f\0\0\x1\xd6\0\0\0\r\0\0\0\x64\0\0\x4P\0\0\0\xe\x1\x1\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x64\xff\xff\xff\xff\0\0\0\x81\0\0\0\0\0\0\0\xe\0\0\0\x34\0\0\0\x1\0\0\0\0\0\0\x1\x46\0\0\0\x1\0\0\0\0\0\0\0*\0\0\0\x1\0\0\0\0\0\0\0G\0\0\0\x1\0\0\0\0\0\0\0?\0\0\0\x1\0\0\0\0\0\0\0\x94\0\0\0\x1\0\0\0\0\0\0\0\x38\0\0\0\x1\0\0\0\0\0\0\0O\0\0\0\x1\0\0\0\0\0\0\0>\0\0\0\x1\0\0\0\0\0\0\0N\0\0\0\x1\0\0\0\0\0\0\0=\0\0\0\x1\0\0\0\0\0\0\0\x42\0\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\x1\0\0\0\0\0\0\x3\xe8\0)">> qBittorrent.ini
+echo qt5\FilesListState="@ByteArray(\0\0\0\xff\0\0\0\0\0\0\0\x1\0\0\0\x1\0\0\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x4\xc6\0\0\0\x5\x1\x1\0\x1\0\0\0\0\0\0\0\0\0\0\0\0\x64\xff\xff\xff\xff\0\0\0\x81\0\0\0\0\0\0\0\x5\0\0\x2\x1f\0\0\0\x1\0\0\0\0\0\0\0T\0\0\0\x1\0\0\0\0\0\0\0\x9f\0\0\0\x1\0\0\0\0\0\0\0l\0\0\0\x1\0\0\0\0\0\0\x1H\0\0\0\x1\0\0\0\0\0\0\x3\xe8\0)">> qBittorrent.ini
+echo Trackers\qt5\TrackerListState="@ByteArray(\0\0\0\xff\0\0\0\0\0\0\0\x1\0\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x4\xa4\0\0\0\x5\x1\0\0\x1\0\0\0\0\0\0\0\0\0\0\0\0\x64\xff\xff\xff\xff\0\0\0\x81\0\0\0\0\0\0\0\x5\0\0\0\x1e\0\0\0\x1\0\0\0\0\0\0\x1\x9a\0\0\0\x1\0\0\0\0\0\0\0H\0\0\0\x1\0\0\0\0\0\0\0'\0\0\0\x1\0\0\0\0\0\0\x2}\0\0\0\x1\0\0\0\0\0\0\x3\xe8\0)">> qBittorrent.ini
