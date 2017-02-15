670bf835cb99b1d746aa86c5d8a44c24e7bdca67
qiiwexc <qiiwexc@icloud.com>
2017-02-16 00:32:11 +0200

    Restore OS detection

--- scripts.js
+++ scripts.js
@@ -1,3 +1,37 @@
+// detect system architecture
+var systemArchitecture;
+if (navigator.userAgent.indexOf('WOW64') !== -1 || navigator.userAgent.indexOf('Win64') !== -1) {
+  systemArchitecture = 64;
+} else {
+  systemArchitecture = 86;
+}
+
+// detect system version
+var systemVersion;
+if (navigator.appVersion.indexOf('Windows NT 10.0') !== -1) {
+  systemVersion = 10;
+} else if (navigator.appVersion.indexOf('Windows NT 6.4') !== -1) {
+  systemVersion = 10;
+} else if (navigator.appVersion.indexOf('Windows NT 6.3') !== -1) {
+  systemVersion = 8.1;
+} else if (navigator.appVersion.indexOf('Windows NT 6.2') !== -1) {
+  systemVersion = 8;
+} else if (navigator.appVersion.indexOf('Windows NT 6.1') !== -1) {
+  systemVersion = 7;
+} else if (navigator.appVersion.indexOf('Windows NT 6.0') !== -1) {
+  systemVersion = 'Vista';
+} else if (navigator.appVersion.indexOf('Windows NT 5.2') !== -1) {
+  systemVersion = 'XP';
+} else if (navigator.appVersion.indexOf('Windows NT 5.1') !== -1) {
+  systemVersion = 'XP';
+} else if (navigator.appVersion.indexOf('Windows NT 5.0') !== -1) {
+  systemVersion = 2000;
+} else if (navigator.appVersion.indexOf('Windows NT 4.0') !== -1) {
+  systemVersion = 'NT';
+} else {
+  systemVersion = '?';
+}
+
 // detect system language
 var systemLanguage;
 if(navigator.browserLanguage) {
@@ -25,6 +59,9 @@ elements = document.querySelectorAll('.' + systemLanguage);
 for (var i = 0; i < elements.length; i++) {
   elements[i].className = systemLanguage;
 }
+
+// display system information
+document.getElementsByClassName('info')[0].innerHTML = (systemVersion === '?' ? 'Unknown Operating System' : 'Windows ' + systemVersion) + ' x' + systemArchitecture + ' | ' + systemLanguage;
 
 // search for drivers
 document.getElementsByTagName('form')[0].onsubmit = function (event) {
